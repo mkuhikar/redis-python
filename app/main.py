ed6ea5cb8f8f9203325e00df51883f8699c0bc8d
mkuhikar <mkuhikar@gmail.com>
2024-09-15 11:10:59 +0530

    pong support

--- app/main.py
+++ app/main.py
@@ -7,8 +7,9 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 6379))
+    client, addr = server_socket.accept() # wait for client
+    client.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
