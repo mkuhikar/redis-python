ef64c41760689d08fd06e921e976571e0ba46aef
mkuhikar <mkuhikar@gmail.com>
2024-09-15 17:41:06 +0530

    testing custom Parser

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 import socket  # noqa: F401
 import asyncio
 from asyncio import Server
+from utility.parser import Parser
 
 async def handle_client(reader,writer):
     
     addr = writer.get_extra_info('peername')
 
-    
-
-    
     client_response = b'+PONG\r\n'
     while(True):
         data = await reader.read(100)
@@ -18,11 +16,19 @@ async def handle_client(reader,writer):
         print(f"Received {messages} from {addr!r}")
         messages = messages.split('\n')
         print(f"Messages {messages}")
-        for message in messages:
-            print(f"Individual message {message}")
-            if 'PING' in message:
-                writer.write(client_response)
-                writer.drain()
+        try:
+            parsed_command = Parser.parse_input(messages)
+            print(f"Parsed RESP command: {parsed_command}")
+        except Exception as e:
+            print(f"Error parsing command: {e}")
+            writer.write(b'-ERR invalid command\r\n')  # Send an error response
+            await writer.drain()
+            continue
+        # for message in messages:
+        #     print(f"Individual message {message}")
+        #     if 'PING' in message:
+        #         writer.write(client_response)
+        #         writer.drain()
 
     print("Close the connection")
     writer.close()
