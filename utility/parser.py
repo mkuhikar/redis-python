73d2b30c038410e56077ed42d75149b2b966aaa7
mkuhikar <mkuhikar@gmail.com>
2024-09-15 18:49:35 +0530

    updated code to handle PING

--- utility/parser.py
+++ utility/parser.py
@@ -25,25 +25,23 @@ class Parser:
         i = 1
         while i < len(lines):
             print(f"Lines {lines}")
-            if lines[i][0] == '$':  # Bulk string
+            if lines[i].startswith('$'):  # Bulk string
                 length = int(lines[i][1:])
                 i += 1  # Move to the next line with the actual data
 
                 # First bulk string is the command
                 if command is None:
                     command = lines[i]
-                    print(f"Command1 {command} argument {argument}")
                 else:
                     # Second bulk string is the argument
                     argument = lines[i]
-                    print(f"Command2 {command} argument {argument}")
                 i += 1  # Move to the next '$' or end
                 if command and argument:
                     break
             else:
                 i += 1
         print(f"Command {command} argument {argument}")
-        if command is None or argument is None:
+        if command is None :
             raise ValueError("Command or argument not found in RESP message")
 
         return command, argument
